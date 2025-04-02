f5b1a7c0ec18f97ee5516cab983818ce101421d1
Bulat Shakirov <shakirov.bulat.contact@gmail.com>
2025-04-02 19:20:46 +0300

    added defaul value annotations

--- EmployeeApp.Backend/Dto/Employee/Request/UpdateEmployeeRequest.cs
+++ EmployeeApp.Backend/Dto/Employee/Request/UpdateEmployeeRequest.cs
@@ -1,12 +1,26 @@
-﻿namespace EmployeeApp.Dto.Employee.Request;
+﻿using System.ComponentModel;
+
+namespace EmployeeApp.Dto.Employee.Request;
 
 public class UpdateEmployeeRequest
 {
     public int Id { get; set; }
+
+    [DefaultValue("Bulat")]
     public string Name { get; set; }
+
+    [DefaultValue("Shakirov")]
     public string Surname { get; set; }
+
+    [DefaultValue("+75553339999")]
     public string Phone { get; set; }
+
+    [DefaultValue(1)]
     public int CompanyId { get; set; }
+
+    [DefaultValue(1)]
     public int PassportId { get; set; }
+
+    [DefaultValue(1)]
     public int DepartmentId { get; set; }
 }
